919b82861364c44296475c5f2795fd9810bd9e38
NNJAMBI <nkaranja7@gmail.com>
2020-12-26 23:47:27 +0300

    add initial project set up for staff list

---
+++ src/test/java/models/StaffTest.java
@@ -0,0 +1,13 @@
+package models;
+
+import junit.framework.TestCase;
+
+public class StaffTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+}
