72a32f38fa9c653de7b9b7bab98505f01e86002e
NNJAMBI <nkaranja7@gmail.com>
2020-12-27 13:03:42 +0300

    add a department class and its corresponding test class

---
+++ src/test/java/models/DepartmentsTest.java
@@ -0,0 +1,9 @@
+package models;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import junit.framework.TestCase;
+
+public class DepartmentsTest {
+
+}
--- src/test/java/models/StaffTest.java
+++ src/test/java/models/StaffTest.java
@@ -1,13 +1,17 @@
 package models;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 import junit.framework.TestCase;
 
-public class StaffTest extends TestCase {
-
-    public void setUp() throws Exception {
-        super.setUp();
+public class StaffTest  {
+    @Test
+    public void NewStaffGetsCreatedCorrectlyCreate_true() throws Exception{
+        Staff staff = setUpNewStaff();
+        assertEquals(true, staff instanceof Staff);
     }
 
-    public void tearDown() throws Exception {
-    }
 }
