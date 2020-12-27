e8948305a84428c6e1d757e9fd0c2a15997c466e
NNJAMBI <nkaranja7@gmail.com>
2020-12-27 18:16:02 +0300

    add the first get route in the App.java file to show the skeleton of the page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,24 @@
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.ObjDoubleConsumer;
+
 import spark.ModelAndView;
+
+
+import models.Departments;
+import models.Staff;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
+
+        get("/",(request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
