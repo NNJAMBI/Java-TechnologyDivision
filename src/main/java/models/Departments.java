9df07c06cb4214799aa379d8f4d8d4bf7f14e401
NNJAMBI <nkaranja7@gmail.com>
2020-12-27 16:27:54 +0300

    add a template directory that will hold all the handlebar files for departments and staff

--- src/main/java/models/Departments.java
+++ src/main/java/models/Departments.java
@@ -25,4 +25,18 @@ public class Departments {
     public void setId(int id) {
         this.id = id;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Departments)) return false;
+        Departments departments = (Departments) o;
+        return id == departments.id &&
+                Objects.equals(name, departments.name);
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(name, id);
+    }
 }
