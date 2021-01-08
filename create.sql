b139dd48bdb04cf56893256120cd8e97941acff4
NNJAMBI <nkaranja7@gmail.com>
2021-01-08 18:17:16 +0300

    refactor the test db

--- create.sql
+++ create.sql
@@ -1,6 +1,5 @@
 CREATE DATABASE technologydivision;
 \c technologydivision;
-CREATE TABLE staff (id SERIAL PRIMARY KEY, description VARCHAR, departmentid INTEGER, staff_id VARCHAR UNIQUE);
-CREATE TABLE roles (id SERIAL PRIMARY KEY, staff_roles VARCHAR);
+CREATE TABLE staff (id SERIAL PRIMARY KEY, description VARCHAR, departmentid INTEGER, staff_id VARCHAR UNIQUE, roles VARCHAR);
 CREATE TABLE departments (id SERIAL PRIMARY KEY, name VARCHAR);
 CREATE DATABASE technologydivision_test WITH TEMPLATE technologydivision;
